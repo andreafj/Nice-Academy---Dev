05da527722002cc82e7ad8dbbc55a6909de0c5d7
andreafj <floresjaldin@gmail.com>
2022-11-11 13:54:32 -0400

    Exercise 2.5

--- javatraining/src/HighSchoolTrainee.java
+++ javatraining/src/HighSchoolTrainee.java
@@ -3,17 +3,23 @@ public class HighSchoolTrainee {
 
         int StudentAge = 24;
         String Midlename = "Andrea";
+        String StudentName = "Noelia Andrea";
+        String LastName = "Flores";
         double Prom= 8.6;
-        char FirstInitial = 'N';
-        char LastInitial = 'A';
+        char FirstInitial = StudentName.charAt(0);
+        char LastInitial = LastName.charAt(0);
         boolean Attendance = true;
 
+        String FirstName = "Noelia";
         System.out.println("My Age is " +StudentAge);
         System.out.println("My Prom is "+Prom);
         System.out.println("My Midlename is "+Midlename);
-        System.out.println("My Firsinitial is  "+FirstInitial);
-        System.out.println("My lastinitial is "+LastInitial);
+        System.out.println("My First Initial is  "+FirstInitial);
+        System.out.println("My Last Initial is "+LastInitial);
         System.out.println("My Attendance is "+Attendance);
+        System.out.println("The student name is  "+StudentName);
+        System.out.println("The student First Name "+FirstName);
+        System.out.println("The student Last Name is "+LastName);
 
     }
 }
