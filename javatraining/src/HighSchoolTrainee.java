80e44282ab383a89eead0a9496fd752abbde63b8
andreafj <floresjaldin@gmail.com>
2022-11-11 13:39:24 -0400

    Exercise 2.2

---
+++ javatraining/src/HighSchoolTrainee.java
@@ -0,0 +1,19 @@
+public class HighSchoolTrainee {
+    public static void  main(String[] args){
+
+        int StudentAge = 24;
+        String Midlename = "Andrea";
+        double Prom= 8.6;
+        char FirstInitial = 'N';
+        char LastInitial = 'A';
+        boolean Attendance = true;
+
+        System.out.println("My Age is " +StudentAge);
+        System.out.println("My Prom is "+Prom);
+        System.out.println("My Midlename is "+Midlename);
+        System.out.println("My Firsinitial is  "+FirstInitial);
+        System.out.println("My lastinitial is "+LastInitial);
+        System.out.println("My Attendance is "+Attendance);
+
+    }
+}
